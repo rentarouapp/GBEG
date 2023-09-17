89ca2883a4c80205d80d35f7ed7262adfadef77a
uejo <rentarou1994@gmail.com>
2023-09-17 15:47:52 +0900

    :white_check_mark: テストコードの追加

--- Tests/GBEGTests/GBEGTests.swift
+++ Tests/GBEGTests/GBEGTests.swift
@@ -1,11 +1,27 @@
 import XCTest
+import Combine
 @testable import GBEG
 
 final class GBEGTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(GBEG().text, "Hello, World!")
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testRequestBookSearchModel() {
+        
+        // Mock
+        let mockRequest_1 = BooksSearchRequest(searchWord: "AAA", maxResults: 20)
+        let mockRequest_2 = BooksSearchRequest(searchWord: "BBB", maxResults: 10)
+        
+        let publisher_1 = APIService().request(with: mockRequest_1)
+        let publisher_2 = APIService().request(with: mockRequest_2)
+        
+        XCTAssertNotNil(publisher_1)
+        XCTAssertNotNil(publisher_2)
     }
 }
